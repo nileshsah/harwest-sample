3dac342d40897d8d8f36609ec303ba52dd338c29
Nilesh Sah <nilesh.sah13@gmail.com>
2020-08-02 14:00:00 +0200

    Add solution for problem `A - Air Conditioner` Link: https://atcoder.jp/contests/abc174/tasks/abc174_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc174/submissions/15588205

---
+++ atcoder/abc174/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+
+    cin.tie(0);
+
+    long long n;
+
+    cin >> n;
+
+    if(n>=30) {
+
+        cout << "Yes" <<'\n';
+
+    } else {
+
+        cout << "No" <<'\n';
+
+    }
+
+    return 0;
+
+}
+
